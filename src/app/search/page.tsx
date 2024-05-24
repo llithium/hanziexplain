cc2d6b585d425b8c24e254b03b6a7a9ba6aa49a7
Nigel <barrowjr0@gmail.com>
2024-05-24 08:53:45 -0400

    Organize search results

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,15 +6,31 @@ export default function Search({
   searchParams: { q: string };
 }) {
   const searchResults = search(searchParams.q);
-  console.log(searchResults);
 
   return (
     <div>
       {searchParams.q &&
         searchResults.map((result) => {
           return (
-            <div className="font-hans" key={result.definitions[0]}>
-              {result.simp}
+            <div
+              className="font-hans"
+              key={result.simp + result.trad + result.pinyin}
+            >
+              <div className="flex items-end gap-2">
+                <span className="text-2xl"> {result.simp}</span>
+                {result.simp !== result.trad && (
+                  <span className="text-2xl"> {result.trad}</span>
+                )}
+                <span>{result.pinyin}</span>
+              </div>
+              {result.definitions.map((definition, i) => {
+                return (
+                  <span key={i + definition}>
+                    {definition}
+                    {i !== result.definitions.length - 1 && ", "}
+                  </span>
+                );
+              })}
             </div>
           );
         })}
