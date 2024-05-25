60aceab6bf52c5354a2bcda5ad37d78f310377f6
Nigel <barrowjr0@gmail.com>
2024-05-25 08:20:22 -0400

    Add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Button } from "@nextui-org/button";
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="fixed inset-0 h-svh w-screen ">
+      <div className="mx-auto flex h-full w-fit flex-col items-center justify-center gap-2">
+        <h2 className="text-xl font-bold text-danger-500">
+          Something went wrong!
+        </h2>
+        <p className="text-center">{error.message}</p>
+        <Button color="default" onClick={reset}>
+          Try again
+        </Button>
+      </div>
+    </div>
+  );
+}
