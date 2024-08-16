c087f170e6a3c838bab65019bc9c2da4a9bc59af
Nigel <barrowjr0@gmail.com>
2024-08-16 09:31:04 -0400

    Update search and loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { TailSpin } from "react-loader-spinner";
+import PuffLoader from "react-spinners/PuffLoader";
 export default function Loading() {
   return (
     <div className="fixed inset-0 flex h-screen w-screen items-center justify-center">
-      <TailSpin color="hsl(var(--foreground))" height={150} width={150} />
+      <PuffLoader color="hsl(var(--foreground))" size={125} />
     </div>
   );
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -35,45 +35,47 @@ export default async function Search({
     res !== null ? ((await res.json()) as SearchResult[]) : [];
 
   return (
-    <ul className="px-2">
-      {searchResults.length > 0 || searchParams.q == "" ? (
-        searchResults.map((result) => {
-          return (
-            <li
-              className="font-hans"
-              key={result.simp + result.trad + result.pinyin}
-            >
-              <Link
-                className="active:opacity-disabled transition-opacity hover:opacity-80"
-                href={`/${result.simp}`}
+    <div className="mx-auto px-2 pb-6 lg:w-10/12 lg:px-0">
+      <ul>
+        {searchResults.length > 0 || searchParams.q == "" ? (
+          searchResults.map((result) => {
+            return (
+              <li
+                className="font-hans"
+                key={result.simp + result.trad + result.pinyin}
               >
-                <div className="flex items-end gap-2">
-                  <Characters result={result} />
-                  <span>{result.pinyin}</span>
-                </div>
-                {result.definitions.map((definition, i) => {
-                  return (
-                    <span key={i + definition}>
-                      {definition}
-                      {i !== result.definitions.length - 1 && ", "}
-                    </span>
-                  );
-                })}
-              </Link>
-              <Separator className="my-2" />
-            </li>
-          );
-        })
-      ) : (
-        <div className="col-span-2 mt-auto flex w-full flex-row justify-center">
-          <h2 className="text-2xl">
-            No results found for{" "}
-            <span className="font-semibold tracking-wider">
-              {searchParams.q}
-            </span>
-          </h2>
-        </div>
-      )}
-    </ul>
+                <Link
+                  className="active:opacity-disabled transition-opacity hover:opacity-80"
+                  href={`/${result.simp}`}
+                >
+                  <div className="flex items-end gap-2">
+                    <Characters result={result} />
+                    <span>{result.pinyin}</span>
+                  </div>
+                  {result.definitions.map((definition, i) => {
+                    return (
+                      <span key={i + definition}>
+                        {definition}
+                        {i !== result.definitions.length - 1 && ", "}
+                      </span>
+                    );
+                  })}
+                </Link>
+                <Separator className="my-2" />
+              </li>
+            );
+          })
+        ) : (
+          <div className="col-span-2 mt-auto flex w-full flex-row justify-center">
+            <h2 className="text-2xl">
+              No results found for{" "}
+              <span className="font-semibold tracking-wider">
+                {searchParams.q}
+              </span>
+            </h2>
+          </div>
+        )}
+      </ul>
+    </div>
   );
 }
