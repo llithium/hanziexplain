a3d136d0378c24db65642e74453cec5a6f0fba53
Nigel <barrowjr0@gmail.com>
2024-08-18 07:14:09 -0400

    Change loader color

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import PuffLoader from "react-spinners/PuffLoader";
 export default function Loading() {
   return (
     <div className="fixed inset-0 flex h-screen w-screen items-center justify-center">
-      <PuffLoader color="hsl(var(--foreground))" size={125} />
+      <PuffLoader color="hsl(var(--primary))" size={125} />
     </div>
   );
 }
