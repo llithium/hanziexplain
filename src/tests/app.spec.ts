e793ad9948148bcc6476fc164c360d8384a879e8
Nigel <barrowjr0@gmail.com>
2024-08-29 12:05:45 -0400

    Add test for entry buttons

--- src/tests/app.spec.ts
+++ src/tests/app.spec.ts
@@ -74,56 +74,82 @@ test("Data table sort", async ({ page }) => {
   await expect(page.getByText("Archaic variant of 五[wǔ]")).toBeVisible();
 });
 
-test("Examples section", async ({ page }) => {
-  await page.goto("/做");
+test.describe("Examples", () => {
+  test("Examples section", async ({ page }) => {
+    await page.goto("/做");
 
-  await expect(page.getByText("我无法取代她做英语老师。")).not.toBeVisible();
+    await expect(page.getByText("我无法取代她做英语老师。")).not.toBeVisible();
 
-  await expect(
-    page.getByText("I can't take the place of her as an English teacher."),
-  ).not.toBeVisible();
+    await expect(
+      page.getByText("I can't take the place of her as an English teacher."),
+    ).not.toBeVisible();
 
-  await page.getByRole("button", { name: "Examples Toggle" }).click();
+    await page.getByRole("button", { name: "Examples Toggle" }).click();
 
-  await expect(page.getByText("我无法取代她做英语老师。")).toBeVisible();
+    await expect(page.getByText("我无法取代她做英语老师。")).toBeVisible();
 
-  await expect(
-    page.getByText("I can't take the place of her as an English teacher."),
-  ).toBeVisible();
+    await expect(
+      page.getByText("I can't take the place of her as an English teacher."),
+    ).toBeVisible();
 
-  await page.getByRole("button", { name: "Examples Toggle" }).click();
+    await page.getByRole("button", { name: "Examples Toggle" }).click();
 
-  await expect(
-    page.getByText("I can't take the place of her as an English teacher."),
-  ).not.toBeVisible();
+    await expect(
+      page.getByText("I can't take the place of her as an English teacher."),
+    ).not.toBeVisible();
+  });
+
+  test("Examples page", async ({ page }) => {
+    await page.goto("/做");
+
+    await expect(page.getByText("我无法取代她做英语老师。")).not.toBeVisible();
+
+    await expect(
+      page.getByText("I can't take the place of her as an English teacher."),
+    ).not.toBeVisible();
+
+    await page.getByRole("button", { name: "Examples Toggle" }).click();
+
+    await expect(page.getByText("我无法取代她做英语老师。")).toBeVisible();
+
+    await page.getByRole("link", { name: "View All Examples." }).click();
+
+    await expect(page).toHaveTitle(/做 Examples · Hanzi Explain/);
+
+    await expect(
+      page.getByRole("heading", { name: "Components" }),
+    ).not.toBeVisible();
+
+    await expect(
+      page.getByText("I can't take the place of her as an English teacher."),
+    ).toBeVisible();
+
+    await expect(
+      page.getByText("To do, To make, To produce, To write,"),
+    ).toBeVisible();
+  });
 });
 
-test("Examples page", async ({ page }) => {
-  await page.goto("/做");
+test("Entry switching", async ({ page }) => {
+  await page.goto("/云");
 
-  await expect(page.getByText("我无法取代她做英语老师。")).not.toBeVisible();
+  await expect(page.getByText("Definition: Rain")).not.toBeVisible();
 
-  await expect(
-    page.getByText("I can't take the place of her as an English teacher."),
-  ).not.toBeVisible();
+  await page.getByRole("switch").click();
+
+  await expect(page.getByText("Definition: Rain")).toBeVisible();
+
+  await page.getByRole("button").nth(2).click();
+
+  await expect(page.getByText("Definition: Rain")).not.toBeVisible();
 
   await page.getByRole("button", { name: "Examples Toggle" }).click();
-
-  await expect(page.getByText("我无法取代她做英语老师。")).toBeVisible();
 
   await page.getByRole("link", { name: "View All Examples." }).click();
 
-  await expect(page).toHaveTitle(/做 Examples · Hanzi Explain/);
+  await expect(page.getByText("天上没有云。")).toBeVisible();
 
-  await expect(
-    page.getByRole("heading", { name: "Components" }),
-  ).not.toBeVisible();
+  await page.getByRole("button").nth(2).click();
 
-  await expect(
-    page.getByText("I can't take the place of her as an English teacher."),
-  ).toBeVisible();
-
-  await expect(
-    page.getByText("To do, To make, To produce, To write,"),
-  ).toBeVisible();
+  await expect(page.getByText("天上没有云。")).not.toBeVisible();
 });
